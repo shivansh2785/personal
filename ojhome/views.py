1dd44f7a135dc37e91eba99b81181b3741086b90
shivansh2785 <84847119+shivansh2785@users.noreply.github.com>
2022-06-12 13:25:55 +0530

    Update view.py

--- ojhome/views.py
+++ ojhome/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the oj home index.")
+    return HttpResponse("Hello, world. You're at the oj home index. bye bye")
